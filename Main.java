d139891014f1fb30dc9f1fc14282cfba5812c504
dwcofo <112203502+dwcofo@users.noreply.github.com>
2023-04-28 09:31:32 -0500

    feat: did the assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Main {
-{   
+  
    public static void main(String[] args) 
    {  
       final int QUARTS_IN_GALLON = 4;
@@ -8,8 +8,16 @@ class Main {
       int gallonsNeeded;
       int extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
+     while(true){
       System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
+       try{
+          quartsNeeded = Integer.parseInt(input.next());
+       } catch (Exception e){
+         System.out.println("You need to enter a number!");
+         continue;
+       }
+       break;
+     }
       gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
       extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
       System.out.println("A job that needs " + quartsNeeded +
